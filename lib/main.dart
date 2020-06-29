f87c350ccbbbdc15d87ee16ae0ecd2e80a75a7bb
Tbrahn <tbranham9323@gmail.com>
2020-06-29 00:42:40 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TannersApp());
+}
+
+class TannersApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello!'),);
+  }
+}
