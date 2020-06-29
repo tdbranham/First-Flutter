e51bdf395011349aa59ffadd05f69a190f9530b7
Tbrahn <tbranham9323@gmail.com>
2020-06-29 01:57:19 -0400

    Adding basic styling widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,12 @@ void main() {
 }
 
 class TannersApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(home: Scaffold(
+        appBar: AppBar(title: Text('My First App'),), 
+        body: Text('This is some default text!'),
+      ),
+    );
   }
 }
