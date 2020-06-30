7422d3dce582e3c750b0394a2ad3dac2b61d7cb2
Tbrahn <tbranham9323@gmail.com>
2020-06-30 02:28:48 -0400

    add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,60 @@ void main() {
   runApp(TannersApp());
 }
 
-class TannersApp extends StatelessWidget {
+class TannersApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TannersAppState();
+  }
+}
+
+class TannersAppState extends State<TannersApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-        appBar: AppBar(title: Text('My First App'),), 
-        body: Text('This is some default text!'),
+    var questions = [
+      'What US region did you grow up in?',
+      'Where is your dream city?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Midwest'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('East Coast'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('South'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('West Coast'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
