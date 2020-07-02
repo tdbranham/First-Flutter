1116989c42cd016e04339448e1711dacd13b161c
Tbrahn <tbranham9323@gmail.com>
2020-07-01 22:05:53 -0400

    change text color, add answers function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(TannersApp());
@@ -42,22 +43,9 @@ class _TannersAppState extends State<TannersApp> {
             Question(
               questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Midwest'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('East Coast'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('South'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('West Coast'),
-              onPressed: _answerQuestion,
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
